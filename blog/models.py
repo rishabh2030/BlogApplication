03570c6949e801b8957c018791fa4588f3175790
Rishabh Sharma <rishabhsharma140323@gmail.com>
2023-06-27 15:14:14 +0530

    Model manage which helps to manage the models data's

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                                        .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -16,6 +21,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    published = PublishedManager() 
 
     class Meta:
             ordering = ['-publish']
